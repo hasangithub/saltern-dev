93ad7b39027c1bdab922c308b49a632e00dfd49e
hasan <hasanbannah@gmail.com>
2024-11-01 17:43:40 +0530

    update navbar

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
   <div class="float-right d-none d-sm-inline">
-    v3.2
+    v1.0.0
   </div>
   <strong>Copyright &copy; 2014-2022 <a href="https://dev.puttalamsalterns.org/">puttalamsalterns.org</a>.</strong> All rights reserved.
 </footer>
