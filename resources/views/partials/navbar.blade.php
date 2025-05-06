f86fc845c9a1c2531a49364f59d9c5fb99deebbc
hasan <hasanbannah@gmail.com>
2025-05-06 10:26:04 +0530

    navbar adding username

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,5 +6,23 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
     </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <!-- User Info -->
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                @if(auth('web')->check())
+                    {{ auth('web')->user()->name }}
+                @elseif(auth('owner')->check())
+                    {{ auth('owner')->user()->full_name }}
+                @elseif(auth('buyer')->check())
+                    {{ auth('buyer')->user()->name }}
+                @else
+                    Guest
+                @endif
+            </a>
+        </li>
+    </ul>
 </nav>
 <!-- /.navbar -->
