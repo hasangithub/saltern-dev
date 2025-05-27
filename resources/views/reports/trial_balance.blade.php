89e1e7dad703811f1de133e27b6568d7133e6f98
hasan <hasanbannah@gmail.com>
2025-05-27 12:50:00 +0530

    table

--- resources/views/reports/trial_balance.blade.php
+++ resources/views/reports/trial_balance.blade.php
@@ -3,44 +3,52 @@
 {{-- Customize layout sections --}}
 
 @section('subtitle', 'Welcome')
-@section('content_header_title', 'Payrolls')
-@section('content_header_subtitle', 'Owner Loans')
+@section('content_header_title', 'Reports')
+@section('content_header_subtitle', 'Trial Balance')
 
 {{-- Content body: main page content --}}
 
 @section('content_body')
 <div class="container-fluid">
     <div class="row">
-    <div class="container">
-    <h2>Trial Balance</h2>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Account Group</th>
-                <th>Sub Group</th>
-                <th>Ledger</th>
-                <th class="text-end">Debit</th>
-                <th class="text-end">Credit</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($trialData as $row)
-                <tr>
-                    <td>{{ $row['group'] }}</td>
-                    <td>{{ $row['sub_group'] }}</td>
-                    <td>{{ $row['ledger'] }}</td>
-                    <td class="text-end">{{ number_format($row['debit'], 2) }}</td>
-                    <td class="text-end">{{ number_format($row['credit'], 2) }}</td>
-                </tr>
-            @endforeach
-            <tr class="fw-bold">
-                <td colspan="3" class="text-end">Total</td>
-                <td class="text-end">{{ number_format($totalDebit, 2) }}</td>
-                <td class="text-end">{{ number_format($totalCredit, 2) }}</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Trial Balance</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table class="table table-sm">
+                        <thead>
+                            <tr>
+                                <th>Account Group</th>
+                                <th>Sub Group</th>
+                                <th>Ledger</th>
+                                <th class="text-end">Debit</th>
+                                <th class="text-end">Credit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($trialData as $row)
+                            <tr>
+                                <td>{{ $row['group'] }}</td>
+                                <td>{{ $row['sub_group'] }}</td>
+                                <td>{{ $row['ledger'] }}</td>
+                                <td class="text-end">{{ number_format($row['debit'], 2) }}</td>
+                                <td class="text-end">{{ number_format($row['credit'], 2) }}</td>
+                            </tr>
+                            @endforeach
+                            <tr class="fw-bold">
+                                <td colspan="3" class="text-end">Total</td>
+                                <td class="text-end">{{ number_format($totalDebit, 2) }}</td>
+                                <td class="text-end">{{ number_format($totalCredit, 2) }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
     </div>
 </div>
 @stop
