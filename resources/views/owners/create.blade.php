871f6ef2479fb130dac7ae4d307a7c9cc6421c12
hasan <hasanbannah@gmail.com>
2025-06-12 10:01:58 +0530

    check

--- resources/views/owners/create.blade.php
+++ resources/views/owners/create.blade.php
@@ -14,7 +14,7 @@
         <div class="col-12">
             <div class="card card-default">
                 <div class="card-header">
-                    <h3 class="card-title">Create new owner</h3>
+                    <h3 class="card-title">Create new owner,,</h3>
                     <div class="card-tools">
                         <button type="button" class="btn btn-tool" data-card-widget="collapse">
                             <i class="fas fa-minus"></i>
