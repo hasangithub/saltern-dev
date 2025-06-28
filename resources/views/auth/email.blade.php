9f40eefb4645ff195a2d441c8fd00f0c443fa4cf
hasan <hasanbannah@gmail.com>
2025-06-28 22:14:40 +0530

    fp

--- resources/views/auth/email.blade.php
+++ resources/views/auth/email.blade.php
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>Admin</b>Login</a>
+            <a href=""><b>Forgot Password</a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
