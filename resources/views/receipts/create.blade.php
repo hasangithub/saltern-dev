d36924dddcd1beb3e087d395d108d1199b66708e
hasan <hasanbannah@gmail.com>
2025-06-28 19:30:02 +0530

    employee

--- resources/views/receipts/create.blade.php
+++ resources/views/receipts/create.blade.php
@@ -15,12 +15,20 @@
             <h3 class="card-title">Buyer</h3>
         </div>
 
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
         @if(session('success'))
         <div class="alert alert-success">
             {{ session('success') }}
         </div>
         @endif
-
         <div class="card-body">
             <form method="GET" action="{{ route('receipts.create') }}">
                 <label>Select Buyer:</label>
