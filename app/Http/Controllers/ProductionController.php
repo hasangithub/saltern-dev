872fefbd5a92748449fdbbdecde577f55240b643
hasan <hasanbannah@gmail.com>
2025-07-17 12:16:45 +0530

    6

--- app/Http/Controllers/ProductionController.php
+++ app/Http/Controllers/ProductionController.php
@@ -37,7 +37,7 @@ class ProductionController extends Controller
     }
 
     $productions = $query->get();
-dd($request->filled('saltern_id'));
+dd($request->saltern_id);
         return view('productions.index', compact('productions', 'salterns'));
     }
 }
