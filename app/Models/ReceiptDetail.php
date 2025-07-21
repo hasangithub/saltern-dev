52d5afa37066c6aa3138344768de13de836ab8b3
hasan <hasanbannah@gmail.com>
2025-07-21 15:34:38 +0530

    print

--- app/Models/ReceiptDetail.php
+++ app/Models/ReceiptDetail.php
@@ -17,4 +17,19 @@ class ReceiptDetail extends Model
     {
         return $this->belongsTo(Receipt::class);
     }
+
+    public function getOwnerAttribute()
+    {
+        if ($this->entry_type === 'weighbridge') {
+            $entry = WeighbridgeEntry::find($this->entry_id);
+            return $entry->owner;
+        }
+
+        if ($this->entry_type === 'loan') {
+            $entry = OwnerLoanRepayment::find($this->entry_id);
+            return $entry->ownerLoan->membership->owner;
+        }
+
+        return null;
+    }
 }
