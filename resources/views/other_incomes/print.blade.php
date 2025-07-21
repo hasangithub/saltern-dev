0a2492098b98a8b75c45245569123948ad8ae682
hasan <hasanbannah@gmail.com>
2025-07-21 11:44:34 +0530

    re

--- resources/views/other_incomes/print.blade.php
+++ resources/views/other_incomes/print.blade.php
@@ -6,141 +6,140 @@
     <title>Other Income Receipt</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        @media print {
-            @page {
-                size: A6 portrait;
-                margin: 0;
-            }
+    @page {
+        margin: 0cm;
+        size: A6 portrait;
+    }
 
-            body {
-                margin: 0;
-                padding: 0;
-            }
+    body {
+        font-family: "Times New Roman", Times, serif;
+        font-size: 11px;
+        line-height: 1.3;
+        margin: 0;
+        padding: 10;
+    }
 
-            .no-print {
-                display: none !important;
-            }
-        }
+    .center {
+        text-align: center;
+    }
 
-        body {
-            font-family: "Times New Roman", Times, serif;
-            font-size: 11px;
-            line-height: 1.3;
-            margin: 0;
-        }
+    .header {
+        text-align: center;
+        margin-bottom: 12px;
+    }
 
-        .center {
-            text-align: center;
-        }
+    .header-title {
+        font-size: 12px;
+        font-weight: bold;
+        letter-spacing: 0.5px;
+        text-transform: uppercase;
+    }
 
-        .header {
-            text-align: center;
-            margin-bottom: 12px;
-        }
+    .sub-header {
+        font-size: 11.5px;
+        color: #333;
+        margin-top: 2px;
+    }
 
-        .header-title {
-            font-size: 10px;
-            font-weight: bold;
-            letter-spacing: 0.5px;
-            text-transform: uppercase;
-        }
+    .section-divider {
+        border-top: 1px dashed #000;
+        margin: 6px 0;
+    }
 
-        .sub-header {
-            font-size: 11px;
-            color: #333;
-            margin-top: 2px;
-        }
+    .section-title {
+        font-size: 12px;
+        text-decoration: underline;
+    }
 
-        .section-divider {
-            border-top: 1px dashed #000;
-            margin: 6px 0;
-        }
+    .details {
+        margin-bottom: 1px;
+    }
 
-        .section-title {
-            font-size: 12px;
-            text-decoration: underline;
-        }
+    .details table {
+        width: 100%;
+        border-collapse: collapse;
+    }
 
-        .details {
-            margin-bottom: 1px;
-        }
+    .details td {
+        padding: 1px 3px;
+        vertical-align: top;
+        font-size: 14px;
+    }
 
-        .details table {
-            width: 100%;
-            border-collapse: collapse;
-        }
+    .footer {
+        margin-top: 16px;
+    }
 
-        .details td {
-            padding: 1px 3px;
-            vertical-align: top;
-            font-size: 14px;
-        }
+    .signature {
+        float: right;
+        text-align: center;
+        margin-top: 15px;
+    }
 
-        .footer {
-            margin-top: 16px;
-        }
+    .line {
+        border-top: 1px solid #000;
+        margin-top: 20px;
+        width: 100px;
+    }
 
-        .signature {
-            float: right;
-            text-align: center;
-            margin-top: 15px;
-        }
+    .amount-right {
+        text-align: right;
+        font-weight: bold;
+    }
 
-        .line {
-            border-top: 1px solid #000;
-            margin-top: 20px;
-            width: 100px;
-        }
-
-        .amount-right {
-            text-align: right;
-            font-weight: bold;
-        }
+    .content-wrapper {
+        width: 100%;
+        padding: 10px;
+        border: 1px;
+        box-sizing: border-box;
+    }
     </style>
 </head>
 
 <body @if(!isset($from_pdf)) onload="window.print()" @endif>
-    <div class="header">
-        <div class="header-title">PUTTALAM SALT PRODUCERS WELFARE SOCIETY LTD</div>
-        <div class="sub-header">Reg No: S/6709 &nbsp;&nbsp;|&nbsp;&nbsp; Tel/Fax: 032 2265260</div>
-        <div class="section-divider"></div>
-        <div class="section-title">OTHER INCOME</div>
-    </div>
-
-    <div class="details">
-        <table>
-            <tr>
-                <td><strong>OtherIncome Id:</strong></td>
-                <td>{{ $income->id }}</td>
-            </tr>
-            <tr>
-                <td><strong>Date:</strong></td>
-                <td>{{ \Carbon\Carbon::parse($income->received_date)->format('Y-m-d') }}</td>
-            </tr>
-            <tr>
-                <td><strong>Buyer:</strong></td>
-                <td>{{ $income->buyer->full_name }}</td>
-            </tr>
-            <tr>
-                <td><strong>Category:</strong></td>
-                <td>{{ $income->incomeCategory->name }}</td>
-            </tr>
-            <tr>
-                <td><strong>Description:</strong></td>
-                <td>{{ $income->description }}</td>
-            </tr>
-            <tr>
-                <td><strong>Amount:</strong></td>
-                <td>Rs. {{ number_format($income->amount, 2) }}</td>
-            </tr>
-        </table>
-    </div>
-    <div class="footer">
-        <div class="signature">
-            <div class="line"></div>
-            <p>Cashier</p>
+    <div class="content-wrapper">
+        <div class="header">
+            <div class="header-title">PUTTALAM SALT PRODUCERS WELFARE SOCIETY LTD</div>
+            <div class="sub-header">Reg No: S/6709 &nbsp;&nbsp;|&nbsp;&nbsp; Tel/Fax: 032 2265260</div>
+            <div class="section-divider"></div>
+            <div class="section-title">OTHER INCOME</div>
         </div>
-    </div> 
+
+        <div class="details">
+            <table>
+                <tr>
+                    <td><strong>OtherIncome Id:</strong></td>
+                    <td>{{ $income->id }}</td>
+                </tr>
+                <tr>
+                    <td><strong>Date:</strong></td>
+                    <td>{{ \Carbon\Carbon::parse($income->received_date)->format('Y-m-d') }}</td>
+                </tr>
+                <tr>
+                    <td><strong>Buyer:</strong></td>
+                    <td>{{ $income->buyer->full_name }}</td>
+                </tr>
+                <tr>
+                    <td><strong>Category:</strong></td>
+                    <td>{{ $income->incomeCategory->name }}</td>
+                </tr>
+                <tr>
+                    <td><strong>Description:</strong></td>
+                    <td>{{ $income->description }}</td>
+                </tr>
+                <tr>
+                    <td><strong>Amount:</strong></td>
+                    <td>Rs. {{ number_format($income->amount, 2) }}</td>
+                </tr>
+            </table>
+        </div>
+        <div class="footer">
+            <div class="signature">
+                <div class="line"></div>
+                <p>Cashier</p>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
