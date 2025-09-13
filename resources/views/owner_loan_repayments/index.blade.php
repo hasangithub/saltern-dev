c1b2249bf303a39bf1f10993c7d5bf505196d66a
hasan <hasanbannah@gmail.com>
2025-09-14 03:04:16 +0530

    no message

--- resources/views/owner_loan_repayments/index.blade.php
+++ resources/views/owner_loan_repayments/index.blade.php
@@ -68,7 +68,7 @@
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-danger btn-sm"
-                                                onclick="return confirm('Are you sure you want to delete this repayment?')">
+                                                onclick="return confirm('Are you sure you want to delete this repayment#{{ $repayment->id }}?')">
                                                 Delete
                                             </button>
                                         </form>
