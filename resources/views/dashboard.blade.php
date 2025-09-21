37d84524ef615d77c3562cf9c322976deb8778d9
hasan <hasanbannah@gmail.com>
2025-09-22 01:55:42 +0530

    no message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,26 +11,78 @@
 @section('content_body')
 <div class="container-fluid">
     <div class="row">
+        {{-- Today --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ number_format($totalServiceChargeToday, 2) }}</h3>
+                    <p>Service Charge (Today)</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-coins"></i>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $totalWeighbridgeEntriesToday }}</h3>
+                    <p>Weighbridge Entries (Today)</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-truck"></i>
+                </div>
+            </div>
+        </div>
+
+        {{-- This Week --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ number_format($totalServiceChargeWeek, 2) }}</h3>
+                    <p>Service Charge (This Week)</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-coins"></i>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $totalWeighbridgeEntriesWeek }}</h3>
+                    <p>Weighbridge Entries (This Week)</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-truck"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
         <div class="col-lg-6">
             <div class="card">
-               
+
             </div>
             <!-- /.card -->
 
             <div class="card">
-    
+
             </div>
             <!-- /.card -->
         </div>
         <!-- /.col-md-6 -->
         <div class="col-lg-6">
             <div class="card">
-               
+
             </div>
             <!-- /.card -->
 
             <div class="card">
-                
+
             </div>
         </div>
         <!-- /.col-md-6 -->
