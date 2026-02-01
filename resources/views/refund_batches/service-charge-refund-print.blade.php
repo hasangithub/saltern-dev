ce0c188b59c817b57cc779f9fdabc2d8eb92f05d
hasan <hasanbannah@gmail.com>
2026-02-01 16:35:55 +0530

    no message

--- resources/views/refund_batches/service-charge-refund-print.blade.php
+++ resources/views/refund_batches/service-charge-refund-print.blade.php
@@ -3,44 +3,45 @@
 @section('content')
 
 @foreach($grouped as $yahaiId => $salterns)
-    @php
-        $firstRefund = $salterns->first()->first();
-        $yahaiName = optional($firstRefund->memberships->saltern->yahai)->name ?? 'Unknown Yahai';
-    @endphp
+@php
+$firstRefund = $salterns->first()->first();
+$yahaiName = optional($firstRefund->memberships->saltern->yahai)->name ?? 'Unknown Yahai';
+@endphp
 
-    <h3>Yahai: {{ $yahaiName }}</h3>
+<h3>Yahai: {{ $yahaiName }}</h3>
 
-    @foreach($salterns as $salternId => $refunds)
-        @php
-            $salternName = optional($refunds->first()->memberships->saltern)->name ?? 'Unknown Saltern';
-        @endphp
+@foreach($salterns as $salternId => $refunds)
+@php
+$salternName = optional($refunds->first()->memberships->saltern)->name ?? 'Unknown Saltern';
+@endphp
 
-        <table>
-            <thead>
-                <tr>
-                    <th>Owner</th>
-                    <th>Waikkal No</th>
-                    <th>Voucher ID</th>
-                    <th>Refund Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($refunds as $refund)
-                <tr>
-                    <td>{{ optional($refund->memberships->owner)->name_with_initial ?? '-' }}</td>
-                    <td>{{ $refund->memberships->saltern->name ?? '-' }}</td>
-                    <td>{{ $refund->voucher_id ?? '-' }}</td>
-                    <td class="text-right">{{ number_format($refund->refund_amount,2) }}</td>
-                </tr>
-                @endforeach
+<table>
+    <thead>
+        <tr>
+            <th>Owner</th>
+            <th>Waikkal No</th>
+            <th>Voucher ID</th>
+            <th>Refund Amount</th>
+        </tr>
+    </thead>
+    <tbody>
 
-                <tr class="subtotal">
-                    <td colspan="3">Subtotal - {{ $salternName }}</td>
-                    <td class="text-right">{{ number_format($refunds->sum('refund_amount'),2) }}</td>
-                </tr>
-            </tbody>
-        </table>
-    @endforeach
+        @foreach($refunds as $refund)
+        <tr>
+            <td>{{ optional($refund->memberships->owner)->name_with_initial ?? '-' }}</td>
+            <td>{{ $refund->memberships->saltern->name ?? '-' }}</td>
+            <td>{{ $refund->voucher_id ?? '-' }}</td>
+            <td class="text-right">{{ number_format($refund->refund_amount,2) }}</td>
+        </tr>
+        @endforeach
 
+        <tr class="subtotal">
+            <td colspan="3">Subtotal - {{ $salternName }}</td>
+            <td class="text-right">{{ number_format($refunds->sum('refund_amount'),2) }}</td>
+        </tr>
+
+        @endforeach
+    </tbody>
+</table>
 @endforeach
 @endsection('content')
